30015a22ecebba7df7224586ff288f7be0746252
Kartik Arora <chipset95@yahoo.co.in>
2018-01-12 22:57:38 +0530

    added wai-aria accessibility

--- js/main.js
+++ js/main.js
@@ -151,10 +151,12 @@ createRestaurantHTML = (restaurant) => {
     const image = document.createElement('img');
     image.className = 'restaurant-img';
     image.src = DBHelper.imageUrlForRestaurant(restaurant);
+    image.alt = "restaurant feature image";
     li.append(image);
 
     const name = document.createElement('h1');
     name.innerHTML = restaurant.name;
+    name.tabIndex = 3;
     li.append(name);
 
     const neighborhood = document.createElement('p');
